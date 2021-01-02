3f7f3de9ef2ce4a5cc759b8326a3308c7072e94c
Crowdbotics <team@crowdbotics.com>
2021-01-02 13:39:54 +0000

    Initial commit.

---
+++ ios/smart_lighter_23651Tests/smart_lighter_23651Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface smart_lighter_23651 : XCTestCase
+
+@end
+
+@implementation smart_lighter_23651
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
